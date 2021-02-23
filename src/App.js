5517fcbe8b262d9428f5d1381860008930024427
Vadim2017777 <svanovivan@gmail.com>
2021-02-23 16:24:45 +0200

    Add button, sign-in components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/hompage.component";
 import ShopPage from "./pages/shop/shoppage.component";
+import SigInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 import Header from "./components/header/header.component";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
+				<Route path="/signIn" component={SigInAndSignUpPage} />
 			</Switch>
 		</div>
 	);
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+import "./sign-in.styles.scss";
+
+export class SignIn extends Component {
+	state = { email: "", password: "" };
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		this.setState({ emai: "", password: "" });
+	};
+
+	handleChange = (event) => {
+		const { name, value } = event.target;
+
+		this.setState({ [name]: value });
+	};
+
+	render() {
+		const { email, password } = this.state;
+		return (
+			<div className="sign-in">
+				<h2 className="title">I have already have an account</h2>
+				<span> Sign in with your email or password</span>
+
+				<form onSubmit={this.handleSubmit}>
+					<FormInput
+						name="email"
+						type="email"
+						onChange={this.handleChange}
+						value={email}
+						label="email"
+						required
+					/>
+
+					<FormInput
+						name="password"
+						type="password"
+						onChange={this.handleChange}
+						value={password}
+						label="password"
+						required
+					/>
+
+					<CustomButton type="submit">Submit form</CustomButton>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default SignIn;
