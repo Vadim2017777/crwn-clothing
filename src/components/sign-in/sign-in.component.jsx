011f0faed0801f8413a953cab5384c2f0614ba41
Vadim2017777 <svanovivan@gmail.com>
2021-02-25 19:05:31 +0200

    Add auth.signOut method in sign-component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -47,11 +47,12 @@ export class SignIn extends Component {
 						label="password"
 						required
 					/>
-
-					<CustomButton type="submit">Submit form</CustomButton>
-					<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-						Sign in with Google
-					</CustomButton>
+					<div className="buttons">
+						<CustomButton type="submit">Sign in</CustomButton>
+						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+							Sign in with Google
+						</CustomButton>
+					</div>
 				</form>
 			</div>
 		);
