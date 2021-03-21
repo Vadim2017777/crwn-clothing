98a5a3236105f636380fd9b4ffee30cb88f5351b
Vadim <svanovivan@gmail.com>
2021-03-21 21:19:00 +0200

    Add container pattern to shopPage

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -38,7 +38,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
-export const converCollectionsToSnapshotToMap = (collections) => {
+export const convertCollectionsSnapshotToMap = (collections) => {
   const transformedCollection = collections.docs.map((doc) => {
     const { title, items } = doc.data();
 
